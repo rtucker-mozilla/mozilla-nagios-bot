b6ee7c4441c238bd917a19861cd89752f9a77979
Rob Tucker <rtucker@mozilla.com>
2014-09-15 17:44:32 -0400

    Fixed incorrect onduty type

--- MozillaNagiosStatus.py
+++ MozillaNagiosStatus.py
@@ -1372,7 +1372,7 @@ class MozillaNagiosStatus:
             else:
                 return "UNKNOWN"
         except IndexError:
-            return "ERROR: %s" % onduty_type
+            return "ERROR: %s" % oncall_type
 
     def get_oncall_from_statusmk(self, oncall_type):
         oncall_from_statusmk = self.get_oncall_name_from_statusmk(oncall_type)
